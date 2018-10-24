77053b9c9605a99d68cfe44b5d410fcc1c716a07
Paula Katerinne Campos Campos <pau.campos89@gmail.com>
2018-10-24 08:19:16 -0300

    decimal-number + percent + currency + json pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   title = 'Pipes';
   nombre: String = 'Paula';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
+
+  a: number = 0.234;
+
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 200,
+    direccion: {  
+      calle: "Primera",
+      casa: 19
+    }
+  }
 }
